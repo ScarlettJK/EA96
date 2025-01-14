9f5616f127a2815ae0c04d0519675cf541ef5eb9
Scarlett_JK <abigail_puentes_23@comunidad.unam.mx>
2025-01-13 19:25:02 -0600

    Clase 4 final

--- Clase4.cpp
+++ Clase4.cpp
@@ -6,20 +6,20 @@
 #include <ctype.h>
 #include <string.h>
 
-//Intento 2
+
 
 int countVowels(char* phrase) { //cuenta las vocales de una frase
-int vocales = 0;
+    int vocales = 0;
     for(int i = 0; phrase[i] != '\0'; i++) {
         char letra =  tolower (phrase[i]);
-            if ( letra == 'a' ||
-             letra == 'e' ||
-             letra == 'i' ||
-             letra == 'o' ||
-             letra == 'u'
-             ){
-              vocales++;
-              }
+        if ( letra == 'a' ||
+         letra == 'e' ||
+         letra == 'i' ||
+         letra == 'o' ||
+         letra == 'u'
+         ){
+            vocales++;
+         }
     }
     return vocales;
 }
@@ -27,31 +27,41 @@ int vocales = 0;
 
 
 
-void countConsonants(char* phrase){ //cuenta las consonantes
+int countConsonants(char* phrase){ //cuenta las consonantes
     int consonantes = 0;
     for(int i = 0; phrase[i] != '\0'; i++) {
         char letra =  tolower (phrase[i]);
-            if ( isalpha (letra) && ! (letra == 'a' ||
-             letra == 'e' ||
-             letra == 'i' ||
-             letra == 'o' ||
-             letra == 'u'
-             )){
-              consonantes++;
-              }
+        if ( isalpha (letra) && ! (letra == 'a' ||
+         letra == 'e' ||
+         letra == 'i' ||
+         letra == 'o' ||
+         letra == 'u'
+         )){
+            consonantes++;
+         }
     }
-   // return consonantes;
+    return consonantes;
 }
 
 void getInput(char* phrase){ //captura la frase del usuario
-  printf("Ingrese una frase: \n");
-  scanf("%s", phrase);
+    printf("Ingrese una frase: \n");
+    fgets(phrase, 100, stdin);
+    //phrase[strcspn(phrase, ""\n)] = '\0';
 
 }
 
 
 
 int main(){
-    void getInput();
+    char phrase[100];
+    getInput(phrase);
+
+    int vocales = countVowels(phrase);
+    int consonantes = countConsonants(phrase);
+
+    printf("Numero de vocales: %d\n", vocales);
+    printf("Numero de consonantes: %d\n", consonantes);
+
+    return 0;
 
 }
